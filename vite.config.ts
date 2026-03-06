928145cfeca032cc5afcb44655b4b01dc04a3254
dharunkumar-2005 <dharunkumar0011@gmail.com>
2026-03-06 20:01:41 +0530

    Fixed white screen issue for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: true, // Idhu thaan unga mobile-la portal open aaga vazhi seiyum
     port: 5173
